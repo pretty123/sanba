0e01261437abfd4791c298f31b025d8ed9190eba
pretty123 <820168613@qq.com>
2016-03-09 18:09:58 +0800

    gii登录

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -8,6 +8,7 @@ use yii\web\Controller;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
+use app\models\EntryForm;
 
 class SiteController extends Controller
 {
@@ -107,5 +108,23 @@ class SiteController extends Controller
     }
     public function actionLearn1(){
     	return $this->render('learn1');
+    }
+    public function actionSecret1(){
+    	return $this->render('secret1');
+    }
+    public function actionEntry()
+    {
+    	$model = new EntryForm;
+    
+    	if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+    		// 验证 $model 收到的数据
+    
+    		// 做些有意义的事 ...
+    
+    		return $this->render('entry-confirm', ['model' => $model]);
+    	} else {
+    		// 无论是初始化显示还是数据验证错误
+    		return $this->render('entry', ['model' => $model]);
+    	}
     }
 }
---
+++ views/site/calendar.php
@@ -0,0 +1,14 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'Calendar';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>  
+    <code><?= __FILE__ ?></code>
+</div>
+
