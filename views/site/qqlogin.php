aec6aa45cf6226d7910143621311c85ea82ecb81
pretty123 <820168613@qq.com>
2016-03-10 17:13:32 +0800

    微擎

---
+++ views/site/qqlogin.php
@@ -0,0 +1,20 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'qq第三方登录实现';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+    <p>1.登录qq互联http://connect.qq.com/</p>
+    <p>2.点击管理中心-》创建应用->网站</p>
+    <p></p>
+	<p></p>
+	<p></p>
+	<p></p>
+	<p></p>
+    <code><?= __FILE__ ?></code>
+</div>
---
+++ views/site/secret1.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = '我的秘密';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+<p> 1.http://git.ueware.com/
+    </p>
+    <p>2.帐号：qfy</p>
+　　<p>3.密码：qq号</p>
+　　<p>4.微擎学习http://www.tudou.com/l/fcDyajFvbHw/&iid=230323918&resourceId=0_04_02_99/v.swf</p>
+    <code><?= __FILE__ ?></code>
+</div>
